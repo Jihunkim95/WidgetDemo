d3742bf683f61e4186a6607d1b8fc5168b94e055
Jihunkim95 <darak551@naver.com>
2023-12-28 11:33:15 +0900

    init: 위젯View및 위젯킷 초기 설정
    아이들에게 폭풍우에 대해 가르치기 위해 설계된 날씨 앱 초기 프로토타입

---
+++ WidgetDemo/WidgetDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WidgetDemo
+//
+//  Created by 김지훈 on 2023/12/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WidgetDemo/WidgetDemo/WidgetDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WidgetDemoApp.swift
+//  WidgetDemo
+//
+//  Created by 김지훈 on 2023/12/28.
+//
+
+import SwiftUI
+
+@main
+struct WidgetDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
